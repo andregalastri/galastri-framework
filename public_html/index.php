585ce57e5612cbb528f0596f466b270f4e407786
andregalastri <contato@andregalastri.com.br>
2022-02-27 12:01:35 -0300

    Made it easy to set the project path in index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,12 @@
 <?php
+
+/**
+ * Path that points to your project directory (the same that contains the app and galastri folders).
+ */
+$projectDirectory = '..';
+
 /**
  * Importing the bootstrap file that starts the framework.
  */
-require_once('../galastri/bootstrap.php');
+require_once(ltrim($projectDirectory, '/').'/galastri/bootstrap.php');
+unset($projectDirectory);
